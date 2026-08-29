f6c8516c4f4cf812e35439684077803c0edeec95
RVXv2 <abideen0908827561@gmail.com>
2026-08-30 04:07:02 +0700

    Add files via upload

--- main.lua
+++ main.lua
@@ -4,14 +4,19 @@ local ScriptMap = {
     [4924922222] = "https://raw.githubusercontent.com/RVXv2/RVX-hub/main/games/brookhaven.lua",
 }
 
-local url = ScriptMap[PlaceId]
+local UniversalURL = "https://raw.githubusercontent.com/RVXv2/RVX-hub/main/universal.lua"
+
+local url = ScriptMap[PlaceId] or UniversalURL
 
 local Intro = loadstring(game:HttpGet("https://raw.githubusercontent.com/RVXv2/RVX-hub/main/games/intro.lua"))()
 
 Intro.Show(function()
-    if url then
-        loadstring(game:HttpGet(url))()
-    else
-        warn("RVX Hub ยังไม่รองรับแมพนี้ | PlaceId: " .. tostring(PlaceId))
+    local success, result = pcall(function()
+        return loadstring(game:HttpGet(url))()
+    end)
+
+    if not success then
+        warn("RVX Hub โหลดสคริปต์ไม่สำเร็จ | PlaceId: " .. tostring(PlaceId))
+        warn(result)
     end
 end)
